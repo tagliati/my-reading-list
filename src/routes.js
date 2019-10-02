f5b724079cca0c6cdd29104cf83716b5b68d3547
tagli <tagliati@gmail.com>
2019-10-02 14:29:04 -0300

    Routing first components

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Main from "./pages/Main";
+import Repository from "./pages/Repository";
+import NotFound from "./pages/NotFound";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
