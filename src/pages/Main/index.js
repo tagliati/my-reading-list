be3d0e1d8675245f7543406f511adaabdbae4b92
tagli <tagliati@gmail.com>
2019-10-03 22:34:36 -0300

    Added call to api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,36 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import { Title, ReadingList } from "./styles.js";
+import api from "../../services/api";
 
-export default function Main() {
-  return (
-    <>
-      <Title>Main list </Title>
-      <ReadingList>
-        <li>
-          <img src="http://lorempixel.com/250/400/animals/1/" />
-          <strong>Boku no Code Academia</strong>
-        </li>
-        <li>
-          <img src="http://lorempixel.com/250/400/animals/2/" />
-          <strong>Boku no Code Academia</strong>
-        </li>
-        <li>
-          <img src="http://lorempixel.com/250/400/animals/3/" />
-          <strong>Boku no Code Academia</strong>
-        </li>
-        <li>
-          <img src="http://lorempixel.com/250/400/animals/4/" />
-          <strong>Boku no Code Academia</strong>
-        </li>
-        <li>
-          <img src="http://lorempixel.com/250/400/animals/5/" />
-          <strong>Boku no Code Academia</strong>
-        </li>
-        <li>
-          <img src="http://lorempixel.com/250/400/animals/1/" />
-          <strong>Boku no Code Academia</strong>
-        </li>
-      </ReadingList>
-    </>
-  );
+export default class Main extends Component {
+  state = {
+    mangas: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get("mangalist");
+    this.setState({ mangas: response.data });
+  }
+  render() {
+    const { mangas } = this.state;
+    return (
+      <>
+        <Title>Main list </Title>
+        <ReadingList>
+          {mangas.map(manga => (
+            <li>
+              <img alt="fofo" src="http://lorempixel.com/250/400/animals/1/" />
+              <strong>Boku no Code Academia</strong>
+            </li>
+          ))}
+        </ReadingList>
+      </>
+    );
+  }
 }
---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+const api = axios.create({
+  baseURL: "https://manga-parser-api.herokuapp.com/"
+});
+
+export default api;
